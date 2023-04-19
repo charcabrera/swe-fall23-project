04c79c501de667dadf7f47914bf8083543a4d780
Charlotte Cabrera <charlottecabrera@ou.edu>
2023-04-19 14:20:37 -0500

    added serverside username validity check

--- server/app.js
+++ server/app.js
@@ -1,38 +1,54 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-const app = express()
+const fs = require('fs')
 const port = 8001
-			fs = require('fs')
 
+const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 app.use('/public', express.static(__dirname + '/public'))
 app.use(express.static(__dirname + '/public'))
 
 app.post('/register', (req, res) => {
+  // tests validity of fields
   if(!req.body.user || !req.body.pass) {
     res.send({
       message: 'You must fill out both the user and pass fields.'
     })
     return
   }
-  const re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
-  if(!re.test(req.body.pass)) {
+
+  // tests validity of username
+  const us_re = /^[a-zA-Z[0-9]+$/
+  if(!us_re.test(req.body.user)) {
     res.send({
-      message: `Invalid password. You must match this regular expression: ${re.toString()}`
+      message: `Invalid username. You must match this regular expression: ${us_re.toString()}`
     })
     return
   }
-  userString = req.body.user + ',' + req.body.pass + '\n'
+
+  // tests validity of password
+  const pw_re = /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,}/
+  if(!pw_re.test(req.body.pass)) {
+    res.send({
+      message: `Invalid password. You must match this regular expression: ${pw_re.toString()}`
+    })
+    return
+  }
+
   path = __dirname + '/public/secure.txt'
   f = fs.readFileSync(__dirname + '/public/secure.txt', 'utf8')
-  if(RegExp(req.body.user + ',').test(f)) {
+
+  // tests if the user already exists
+  if(f.includes(req.body.user + ',')) {
     res.send({
       message: `Error: User already exists.`
     })
     return
   }
+
+  // if none of the checks fail, add the user to the file
   fs.appendFile(path, userString, err => {
     if (err) {
       console.error(err);
